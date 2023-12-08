2ab7b620a60d73578281969249578fb95d970fbd
sarathnadendla <sarath@sureify.com>
2023-12-08 22:27:42 +0530

    GenerateFromPassword CompareHashAndPassword

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/go-test
 
 go 1.19
+
+require golang.org/x/crypto v0.16.0
--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
-	"encoding/base64"
-	
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
-
-
 
 func main() {
 
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("sarath:123")))
+	password := "123456789"
 
-	
+	hashPass, err := hashPassword(password)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Hash Password", hashPass)
+	err = comparePassword(password, hashPass)
+	if err != nil {
+		log.Fatalln("Error", err)
+
+	}
+	log.Println("Logged In")
 
 }
 
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generatig bcrypt from hashpassword %w", err)
+	}
+	return bs, nil
+
+}
+
+func comparePassword(password string, hashPassword []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPassword, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid Password: %w", err)
+	}
+	return nil
+}
