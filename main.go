f5e6877209e3d3113b49cf1486280e3dd92186b6
sarathnadendla <sarath@sureify.com>
2023-12-08 15:43:45 +0530

    base64 encoding

--- main.go
+++ main.go
@@ -1,46 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+	"fmt"
+	"encoding/base64"
+	
 )
 
-type person struct {
-	First string
-}
+
 
 func main() {
 
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", boo)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("sarath:123")))
 
-	http.ListenAndServe(":8080", nil)
-
-}
-func foo(w http.ResponseWriter, r *http.Request) {
-
-	p1 := person{
-		First: "Jenny",
-	}
-	p2 := person{
-		First: "James",
-	}
-	people := []person{p1, p2}
-
-	err := json.NewEncoder(w).Encode(people)
-	if err != nil {
-		log.Println("Encoding issue")
-	}
+	
 
 }
-func boo(w http.ResponseWriter, r *http.Request) {
-	p := []person{}
 
-	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-		log.Println("Decoding issue", err)
-	}
-	log.Println("People Data", p)
-
-}
